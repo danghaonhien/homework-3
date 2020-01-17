9417bf76a40e153f90d0a1a3ee4d4b7ed98b3270
danghaonhien <58311274+danghaonhien@users.noreply.github.com>
2020-01-16 20:41:55 -0800

    add array and loop

--- script.js
+++ script.js
@@ -14,16 +14,39 @@ function levelValue(event) {
 
 
 
-//Add function for form
+//Add document for formMain
+let $needUpperCase = document.querySelector("#needUpperCase");
+let $needNumber = document.querySelector("#needNumber");
+let $needSpecialChar = document.querySelector("#needSpecialChar");
 let $showPassword = document.querySelector("#password");
 let $formEl = document.querySelector("#formMain");
+//Add function to formMain
 $formEl.addEventListener('submit', event);
 function event() {
     event.defaultPrevented();
     let characterNumber = $charNumbEl.value;
-    let needUpperCase = document.querySelector("#needUpperCase");
-    let needNumber = document.querySelector("#needNumber");
-    let needSpecialChar = document.querySelector("#needSpecialChar");
-    let password = passwordGenerator(characterNumber, needUpperCase, needNumber, needSpecialChar);
+    //check if box is true//false
+    let needUpperCaseEl = $needUpperCase.checked
+    let needNumberEl = $needUpperCase.checked
+    let needSpecialCharEl = $needUpperCase.checked
+    //create password holder and connect to html passwords
+    let password = passwordGenerator(characterNumber, needUpperCaseEl, needNumberEl, needSpecialCharEl);
     $showPassword.innerText = password;
 }
+//Add function for password
+function passwordGenerator(characterNumber, needUpperCaseEl, needNumberEl, needSpecialCharEl)
+
+//Add CharCode
+
+//Add array and make loop to let passwords push characters through charCode
+
+function arrayAZ(A,Z) {
+    let array = []
+    for (let i = A; i<= Z , i++) {
+        array.push[[i]];
+    }
+    //return array to get value
+    return array;
+}
+
+    //
