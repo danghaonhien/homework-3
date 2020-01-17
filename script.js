d91b0145a05b9d68bd6a75ebf83ed110cb7e9383
danghaonhien <58311274+danghaonhien@users.noreply.github.com>
2020-01-16 20:17:13 -0800

    push script

--- script.js
+++ script.js
@@ -10,6 +10,20 @@ function levelValue(event) {
     let value = event.target.value;
     $levelEl.value = value;
     $charNumbEl.value = value;
-}
+};
 
-??
+
+
+//Add function for form
+let $showPassword = document.querySelector("#password");
+let $formEl = document.querySelector("#formMain");
+$formEl.addEventListener('submit', event);
+function event() {
+    event.defaultPrevented();
+    let characterNumber = $charNumbEl.value;
+    let needUpperCase = document.querySelector("#needUpperCase");
+    let needNumber = document.querySelector("#needNumber");
+    let needSpecialChar = document.querySelector("#needSpecialChar");
+    let password = passwordGenerator(characterNumber, needUpperCase, needNumber, needSpecialChar);
+    $showPassword.innerText = password;
+}
